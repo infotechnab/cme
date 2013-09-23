8cbdbd0c4dc4d63751047527e588174ff603546f
infotechnab <infotechnab@gmail.com>
2013-09-23 17:06:56 +0545

    footer copyright added

--- application/views/cme/templets/footer.php
+++ application/views/cme/templets/footer.php
@@ -1,4 +1,9 @@
- <br/>
+ 
+
         </div>
-    </body>
+
+<div id="footer">
+    <p>Copy right &copy; 2013. All right reserved to <a href="http://cmeremit.com">Chitwan Money Express</a></p></div>
+<br/>
+</body>
 </html>
